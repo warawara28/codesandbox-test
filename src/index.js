1c411008e52f253bc378b13828c8fdc65538bcfb
Yuki Kogawara <kgwryk28@gmail.com>
2022-05-22 00:46:05 +0900

    分割代入を追加

--- src/index.js
+++ src/index.js
@@ -86,3 +86,26 @@
 //   return num1 + num2;
 // };
 // console.log(func3(10, 20));
+
+/**
+ * 分割代入
+ */
+
+// const myProfile = {
+//   name: "山田",
+//   age: 28,
+// }
+// const message1 = `私の名前は${myProfile.name}です。年齢は${myProfile.age}歳です。`;
+// console.log(message1);
+
+// const {name, age} = myProfile;
+// const message2 = `私の名前は${name}です。年齢は${age}歳です。`;
+// console.log(message2);
+
+// const myProfile = ['山田', 28];
+// const message3 = `私の名前は${myProfile[0]}です。年齢は${myProfile[1]}歳です。`;
+// console.log(message3);
+
+// const [name, age] = myProfile;
+// const message4 = `私の名前は${name}です。年齢は${age}歳です。`;
+// console.log(message4);
