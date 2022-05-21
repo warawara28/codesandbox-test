090fef3c3fbd6fb29f91d825c04c42be15646481
Yuki Kogawara <kgwryk28@gmail.com>
2022-05-22 01:05:09 +0900

    map, filterを追加

--- src/index.js
+++ src/index.js
@@ -152,3 +152,24 @@
 
 // arr8[0] = 100;
 // console.log(arr4);
+
+/**
+ * mapやfilterを使った配列の処理
+ */
+// const nameArr = ["田中", "山田", "岡村"];
+// for (let index = 0; index < nameArr.length; index++) {
+//   console.log(nameArr[index]);
+// }
+
+// const nameArr2 = nameArr.map((name) => {
+//   return name;
+// })
+// console.log(nameArr2);
+
+// nameArr.map((name) => console.log(name));
+
+// const numArr = [1, 2, 3, 4, 5];
+// const newNumArr = numArr.filter((num) => {
+//   return num % 2 === 1;
+// });
+// console.log(newNumArr);
