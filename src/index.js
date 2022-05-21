ea13756735d7dbadb8d628cbee039dce81d5273c
Yuki Kogawara <kgwryk28@gmail.com>
2022-05-22 00:42:00 +0900

    アロー関数を追加

--- src/index.js
+++ src/index.js
@@ -62,3 +62,27 @@
 // // テンプレート文字列
 // const message2 = `私の名前は${name}です。年齢は${age}歳です。`;
 // console.log(message2);
+
+/**
+ * アロー関数
+ */
+// 従来の関数
+// function func1(str) {
+//   return str;
+// }
+// const func1 = function(str) {
+//   return str;
+// }
+// console.log(func1("func1です。"));
+
+// // アロー関数
+// // const func2 = (str) => {
+// //   return str
+// // };
+// const func2 = (str) => str;
+// console.log(func2("func2です。"));
+
+// const func3 = (num1, num2) => {
+//   return num1 + num2;
+// };
+// console.log(func3(10, 20));
